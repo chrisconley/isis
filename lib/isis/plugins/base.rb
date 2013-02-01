aac90227ee5b2f685f3bf60bf194954020ae06a7
Chris Conley <chris.m.conley@gmail.com>
2013-02-01 16:04:11 -0500

    separate plugin and add trainview

--- lib/isis/plugins/base.rb
+++ lib/isis/plugins/base.rb
@@ -4,26 +4,10 @@ require 'httparty'
 module Isis
   module Plugin
     class Base
-      def respond_to_msg?(msg, speaker)
-        /\b[!]?isepta\b/i =~ msg
-      end
       def receive_message(msg, speaker, room = nil)
         if respond_to_msg?(msg, speaker)
           @room = room
-          msg = msg.downcase
-          msg = msg.gsub(/\!isepta/, '')
-          from, to = msg.split(' to ').map{|s| s.strip.upcase}
-          response = HTTParty.get("http://isepta-api-v2.herokuapp.com/trains.json?to=#{to}&from=#{from}&day=wk")
-
-          if response.to_a.size > 0
-            departures = response.map do |train|
-              train["departs_at"].gsub(/:00$/, '')
-            end
-            times = departures[0..2].join(', ')
-            "Departing at: #{times}"
-          else
-            'Whoops! Couldn\'t find trains for "msg"'
-          end
+          response
         end
       end
     end
---
+++ lib/isis/plugins/isepta.rb
@@ -0,0 +1,193 @@
+require 'isis/plugins/base'
+require 'httparty'
+
+class Isis::Plugin::Isepta < Isis::Plugin::Base
+  # TODO:
+  # always return the "now" schedule
+  # Allow fuzzy matching on stations
+  # Get current trains
+
+  REGEX = /^\W*\@isepta\b/i
+  def respond_to_msg?(msg, speaker)
+    @msg = msg
+    REGEX =~ @msg
+  end
+
+  def response
+    @msg = @msg.downcase
+    @msg = @msg.gsub(REGEX, '')
+    from, to = @msg.split(' to ').map{|s| s.strip.upcase}
+    response = HTTParty.get("http://isepta-api-v2.herokuapp.com/trains.json?to=#{to}&from=#{from}&day=wk")
+
+    if response.to_a.size > 0
+      train_view = HTTParty.get('http://www3.septa.org/hackathon/TrainView/', :options => { :headers => { 'ContentType' => 'application/json' } })
+      trains = response.map{|t| {departs_at: Time.parse(t['departs_at']), number: t['number']} }
+      trains = trains.select{|t| t[:departs_at] > (Time.now)}[0..2]
+
+      times = trains.map do |train|
+        tv_train = train_view.select{|t| t['trainno'] == train[:number]}.first
+        minutes_late = tv_train && tv_train['late']
+        train[:departs_at].strftime("%I:%M%P (#{minutes_late || "?"})")
+      end.join(", ")
+      "Departing at: #{times}"
+    else
+      "Whoops! Couldn't find trains for '#{@msg}'"
+    end
+  end
+end
+
+STATION_NAMES = {
+"Eastwick" => "EWCK",
+"Trenton" => "TR",
+"Levittown" => "LVTN",
+"Cynwyd" => "CY",
+"Sharon Hill" => "SHAR",
+"Elkins Park" => "ELPK",
+"Wissahickon T. C." => "WSHN",
+"Colmar" => "COLM",
+"Exton" => "EXTN",
+"Warminster" => "WM",
+"Oreland" => "ORLD",
+"Hatboro" => "HATB",
+"Fort Washington" => "FTWA",
+"Meadowbrook" => "MDBK",
+"Willow Grove" => "WLGR",
+"Crestmont" => "CRST",
+"Roslyn" => "ROSL",
+"Ardsley" => "ARDS",
+"Noble" => "NOBL",
+"Rydal" => "RYDL",
+"Whitford" => "WHIT",
+"North Hills" => "NOHL",
+"Folcroft" => "FOLC",
+"Glenolden" => "GLNO",
+"Norwood" => "NORW",
+"Prospect Park-Moore" => "PROS",
+"Ridley Park" => "RDLY",
+"Crum Lynne" => "CRML",
+"Eddystone" => "EDST",
+"Chester" => "CHTC",
+"Highland Avenue" => "HIAV",
+"North Broad" => "NBRD",
+"Darby" => "DARB",
+"Curtis Park" => "CRTS",
+"Newark" => "NK",
+"Fern Rock T.C." => "FRTC",
+"Berwyn" => "BERW",
+"Ardmore" => "ARTC",
+"Bethayres" => "BTHR",
+"Philmont" => "PHLM",
+"Forest Hills" => "FORH",
+"Somerton" => "SOMT",
+"Trevose" => "TREV",
+"Neshaminy Falls" => "NESHAMINY",
+"Langhorne" => "LANG",
+"Woodbourne" => "WDBR",
+"Yardley" => "YDLY",
+"Elwyn" => "EL",
+"Media" => "ME",
+"Primos" => "PRIM",
+"Clifton-Aldan" => "CFTN",
+"Gladstone" => "GLAD",
+"Lansdowne" => "LNSD",
+"Fernwood-Yeadon" => "FERN",
+"Downingtown" => "DOWN",
+"Fortuna" => "FRTU",
+"Melrose Park" => "MLPK",
+"Jenkintown-Wyncote" => "KI",
+"Ambler" => "AMBL",
+"Penllyn" => "PNLN",
+"Gwynedd Valley" => "GWVL",
+"North Wales" => "NWST",
+"Pennbrook" => "PBST",
+"Doylestown" => "DY",
+"Devon" => "DEVN",
+"Wynnewood" => "WYNW",
+"Airport Terminal A East/West" => "A",
+"Thorndale" => "TN",
+"Link Belt" => "LINK",
+"Chalfont" => "CHAL",
+"New Britain" => "NWBT",
+"Del Val College" => "DLVL",
+"Marcus Hook" => "MH",
+"Claymont" => "CLAY",
+"Daylesford" => "DALF",
+"Paoli" => "PA",
+"Haverford" => "HAVE",
+"Strafford" => "STFD",
+"Wayne" => "WAYN",
+"St. Davids" => "STDA",
+"Radnor" => "RADN",
+"Churchmans Crossing" => "CHCR",
+"Villanova" => "VILA",
+"Rosemont" => "ROSE",
+"Bryn Mawr" => "BM",
+"Lansdale" => "LA",
+"Narberth" => "NRST",
+"Merion" => "MERI",
+"Overbrook" => "OVBK",
+"Allegheny" => "ALGY",
+"East Falls" => "EFLS",
+"Manayunk" => "MANY",
+"Ivy Ridge" => "IVYR",
+"Miquon" => "MIQN",
+"Spring Mill" => "SPGM",
+"Conshohocken" => "CONS",
+"Norristown T. C." => "NTC",
+"Main Street" => "MNST",
+"Elm Street, Norristown" => "NT",
+"Wayne Junction" => "WJ",
+"Bristol" => "BRST",
+"Croydon" => "CROY",
+"Eddington" => "EDGT",
+"Airport Terminal B" => "AIRB",
+"Secane" => "SECA",
+"Bala" => "BALA",
+"Wynnefield Avenue" => "WYNF",
+"Airport Terminal C and D" => "AIRC",
+"Tacony" => "TCNY",
+"Bridesburg" => "BRDB",
+"Wister" => "WIST",
+"Germantown" => "GERM",
+"Washington Lane" => "WALA",
+"Stenton" => "STNT",
+"Sedgwick" => "SGWK",
+"Mount Airy" => "MTAI",
+"Wyndmoor" => "WYND",
+"Gravers" => "GRAV",
+"Airport Terminal E" => "AIRE",
+"Chestnut Hill East" => "CE",
+"Chestnut Hill West" => "CW",
+"Highland" => "HIGH",
+"St. Martins" => "STMR",
+"Allen Lane" => "ALLA",
+"Carpenter" => "CARP",
+"Upsal" => "UPSL",
+"Tulpehocken" => "TULP",
+"Chelten Avenue" => "CHEL",
+"Queen Lane" => "QUEN",
+"North Philadelphia" => "NPHR7",
+"30th Street Station" => "30ST",
+"Moylan-Rose Valley" => "MOYL",
+"Wallingford" => "WALF",
+"Swarthmore" => "SWAR",
+"Morton-Rutledge" => "MORT",
+"Angora" => "AGRA",
+"49th Street" => "49TH",
+"Suburban Station" => "SS",
+"Market East Station" => "EM",
+"Temple University" => "TU",
+"Olney" => "OLNY",
+"Lawndale" => "LAWN",
+"West Trenton, NJ" => "WT",
+"Cheltenham" => "CHHM",
+"Ryers" => "RYER",
+"Fox Chase" => "FC",
+"Glenside" => "GS",
+"Malvern" => "MALV",
+"Wilmington" => "WG",
+"University City" => "UC",
+"Cornwells Heights" => "CORN",
+"Torresdale" => "TORR",
+"Holmesburg Junction" => "HOLM"}
+
